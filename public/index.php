c8b60de8df0991433a110d2c92f634c3cb1a08f7
Magali MAI <maimagali26.pro@gmail.com>
2026-03-24 20:08:14 +0100

    back to env dev

--- public/index.php
+++ public/index.php
@@ -6,22 +6,7 @@ use App\Kernel;
 // echo 'APP_DEBUG=' . ($_ENV['APP_DEBUG'] ?? 'not set') . PHP_EOL;
 // exit;
 
-putenv('APP_ENV=prod');     // Force l'environnement
-putenv('APP_DEBUG=0');
-$_ENV['APP_ENV'] = 'prod';
-$_ENV['APP_DEBUG'] = 0;
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-if (php_sapi_name() === 'cli-server') {
-    $url  = parse_url($_SERVER['REQUEST_URI']);
-    $file = __DIR__ . $url['path'];
-
-    // NE TRAITE QUE LES FICHIERS PHP STATIQUES
-    if (is_file($file) && preg_match('/\.php$/', $file)) {
-        return false;
-    }
-}
 
 
 return function (array $context) {
--- start.sh
+++ start.sh
@@ -2,19 +2,11 @@
 
 #!/bin/sh
 
-# Forcer l'environnement prod
-export APP_ENV=prod
-export APP_DEBUG=0
-
 # Installer les dépendances prod
-composer install --no-dev --optimize-autoloader
-
-# Clear le cache prod
-php bin/console cache:clear --env=prod
-php bin/console cache:warmup --env=prod
+composer install
 
 # Utilise le port fourni par Railway
 PORT=${PORT:-8080}
 
 # Lance le serveur PHP intégré
-php -S 0.0.0.0:$PORT public/index.php
+php -S 0.0.0.0:$PORT 
