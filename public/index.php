4d8539744022f34627939978317ee20f1d00719b
Magali MAI <maimagali26.pro@gmail.com>
2026-03-24 18:36:39 +0100

    force prod env

--- public/index.php
+++ public/index.php
@@ -2,19 +2,23 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+// echo 'APP_ENV=' . ($_ENV['APP_ENV'] ?? 'not set') . PHP_EOL;
+// echo 'APP_DEBUG=' . ($_ENV['APP_DEBUG'] ?? 'not set') . PHP_EOL;
+// exit;
 
-echo 'APP_ENV=' . ($_ENV['APP_ENV'] ?? 'not set') . PHP_EOL;
-echo 'APP_DEBUG=' . ($_ENV['APP_DEBUG'] ?? 'not set') . PHP_EOL;
-exit;
+putenv('APP_ENV=prod');     // Force l'environnement
+putenv('APP_DEBUG=0');
+$_ENV['APP_ENV'] = 'prod';
+$_ENV['APP_DEBUG'] = 0;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 if (php_sapi_name() === 'cli-server') {
     $url  = parse_url($_SERVER['REQUEST_URI']);
     $file = __DIR__ . $url['path'];
 
     if (is_file($file)) {
-        // Sert le fichier statique directement
-        return false; // ATTENTION: ne rien mettre après ce return
+        return false; // sert le fichier statique
     }
 }
 
