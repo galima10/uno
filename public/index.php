9316fc500003bd177efe12d06215e3601a8344cf
Magali MAI <maimagali26.pro@gmail.com>
2026-03-24 18:32:24 +0100

    Add logs

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+echo 'APP_ENV=' . ($_ENV['APP_ENV'] ?? 'not set') . PHP_EOL;
+echo 'APP_DEBUG=' . ($_ENV['APP_DEBUG'] ?? 'not set') . PHP_EOL;
+exit;
+
 if (php_sapi_name() === 'cli-server') {
     $url  = parse_url($_SERVER['REQUEST_URI']);
     $file = __DIR__ . $url['path'];
