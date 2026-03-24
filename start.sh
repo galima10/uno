4a5a5f21502892a3071091b00296fc08aab7bae6
Magali MAI <maimagali26.pro@gmail.com>
2026-03-24 17:53:53 +0100

    Add importmap dependency

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+# Installe les dépendances PHP
+composer install --no-dev --optimize-autoloader
+
+# Installe les assets importmap
+php bin/console importmap:install
+
+# Utilise le port fourni par Railway
 PORT=${PORT:-8080}
+
+# Lance le serveur PHP intégré
 php -S 0.0.0.0:$PORT public/router.php
